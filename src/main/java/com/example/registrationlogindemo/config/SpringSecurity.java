8475eb581d3695bcd4cfa55c3e8e63666d584c32
GitJG <cemileqarayeva93@gmail.com>
2023-02-25 17:00:28 +0400

    ver1

--- src/main/java/com/example/registrationlogindemo/config/SpringSecurity.java
+++ src/main/java/com/example/registrationlogindemo/config/SpringSecurity.java
@@ -28,7 +28,7 @@ public class SpringSecurity {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers("/register/**").permitAll()
+                        authorize.requestMatchers("/**").permitAll()
                                 .requestMatchers("/index").permitAll()
                                 .requestMatchers("/users").hasRole("ADMIN")
                 ).formLogin(
